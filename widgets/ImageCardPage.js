425732124a79f3fa8d12d2e162de455fb86193f8
Gautham Sajith <gsajith@umich.edu>
2021-05-27 00:13:12 -0400

    Only do viewheight fix on mobile

--- widgets/ImageCardPage.js
+++ widgets/ImageCardPage.js
@@ -59,12 +59,14 @@ const ImageCardPage = ({
   }, [data]);
 
   useEffect(() => {
-    // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-    let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  }, []);
+    if (isMobile) {
+      // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+      // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+      let vh = window.innerHeight * 0.01;
+      // Then we set the value in the --vh custom property to the root of the document
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    }
+  }, [isMobile]);
 
   return (
     <>
