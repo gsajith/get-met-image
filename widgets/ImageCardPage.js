52cf1645108810684b3cf7bb34b2a87c809309dc
Gautham Sajith <gsajith@umich.edu>
2021-05-26 22:00:40 -0400

    Disable viewport scaling

--- widgets/ImageCardPage.js
+++ widgets/ImageCardPage.js
@@ -69,7 +69,7 @@ const ImageCardPage = ({ data, loading, setUrlDataResult }) => {
             />
             <meta
               name="viewport"
-              content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+              content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
             />
             <link rel="icon" href="/favicon.ico" />
           </Head>
